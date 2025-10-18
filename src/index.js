4db0aaf83309172c4a0e18d8594d53134f31c3ba
Jean <contact@jeanguillet.com>
2025-10-18 12:31:26 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: '*',
+}));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
