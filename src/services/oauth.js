680f65353e80b2d3263f781cf3b7efefb19cd131
Jean <contact@jeanguillet.com>
2025-10-18 17:04:53 +0200

    moved oauth instance into separate file

---
+++ src/services/oauth.js
@@ -0,0 +1,16 @@
+import OAuth from 'oauth-1.0a';
+import CryptoJS from 'crypto-js';
+import config from '../config/index.js';
+
+const oauth = OAuth({
+  consumer: {
+    key: config.consumerKey,
+    secret: config.consumerSecret,
+  },
+  signature_method: 'HMAC-SHA1',
+  hash_function(base_string, key) {
+    return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+  }
+});
+
+export { oauth };
