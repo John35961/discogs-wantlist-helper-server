ce7b892cbcce1cfee0cdd346a74b9c1426564de9
Jean <contact@jeanguillet.com>
2025-10-19 13:52:20 +0200

    handled tokens passed in body

--- src/routes/discogs.js
+++ src/routes/discogs.js
@@ -55,10 +55,9 @@ router.get('/oauth/identity', async (req, res) => {
 });
 
 router.put('/users/:userName/wants/:releaseId', async (req, res) => {
-  const accessToken = req.query.accessToken;
-  const accessTokenSecret = req.query.accessTokenSecret;
   const userName = req.params.userName;
   const releaseId = req.params.releaseId;
+  const { accessToken, accessTokenSecret } = req.body;
 
   if (!accessToken || !accessTokenSecret) {
     return res.status(400).json({ error: 'Missing access tokens' });
