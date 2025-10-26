6af036c6bc4265cd7300efbf544b7b4f1520c44a
Jean <contact@jeanguillet.com>
2025-10-26 13:56:06 +0100

    added notFound middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import V1DiscogsRouter from './routes/v1/index.js';
 import { auth } from './middlewares/auth.middleware.js';
+import { notFound } from './middlewares/notFound.middleware.js';
 import { errorHandler } from './middlewares/errorHandler.middleware.js';
 
 const app = express();
@@ -10,13 +11,10 @@ const app = express();
 app.use(cors({
   origin: '*',
 }));
-
 app.use(express.json());
-
 app.use('/discogs/api/v1', V1DiscogsRouter);
-
+app.use(notFound);
 app.use(auth);
-
 app.use(errorHandler);
 
 app.listen(config.port);
---
+++ src/middlewares/notFound.middleware.js
@@ -0,0 +1,4 @@
+export const notFound = (req, res) => {
+  return res.status(404).json({ error: 'Resource not found' },
+  );
+}
