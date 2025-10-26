535076b8a0dbbb01c83b79a4aaf0495cdbf3c57f
Jean <contact@jeanguillet.com>
2025-10-26 16:45:32 +0100

    respon with access tokens and JWT

--- src/controllers/v1/auth.controller.js
+++ src/controllers/v1/auth.controller.js
@@ -4,11 +4,12 @@ import { ApiError } from '../../utils/apiError.js';
 import oauthService from "../../services/oauth.service.js";
 
 export const verify = async (req, res, next) => {
-  const { accessToken, accessTokenSecret } = req.body;
+  const { requestToken, requestTokenSecret, oauthVerifier } = req.body;
 
-  if (!accessToken || !accessTokenSecret) return (next(new ApiError(400, 'Access tokens missing')));
+  if (!requestToken || !requestTokenSecret) return (next(new ApiError(400, 'Request tokens missing')));
 
   try {
+    const { accessToken, accessTokenSecret } = await oauthService.getAccessToken(requestToken, requestTokenSecret, oauthVerifier);
     const identity = await oauthService.getIdentity(accessToken, accessTokenSecret);
 
     const payload = {
@@ -17,7 +18,11 @@ export const verify = async (req, res, next) => {
     };
     const jwtToken = jwt.sign(payload, config.jwtGlobalSecret, { expiresIn: '6h' });
 
-    res.json({ token: jwtToken });
+    res.json({
+      accessToken: accessToken,
+      accessTokenSecret: accessTokenSecret,
+      jwtToken: jwtToken,
+    });
   } catch (error) {
     next(new ApiError(401, 'Access tokens invalid'));
   };
